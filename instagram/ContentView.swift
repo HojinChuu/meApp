e0e22ceebc52fe8f03786e8b430254ca6408b119
HojinChuu <cnghwls7@gmail.com>
2021-03-25 00:43:50 +0900

    Initial Commit

---
+++ instagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instagram
+//
+//  Created by CHU on 2021/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by CHU on 2021/03/25.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
